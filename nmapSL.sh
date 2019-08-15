5a2da3cbdf60db57661b4887c86b7646c963d064
hiskid123 <hskd123@gmail.com>
2019-08-15 01:25:43 +0000

    Fixed issue with inital scan never terminating, and developed a cleaner way of importing the detected ports into the service detection scan

--- nmapSL.sh
+++ nmapSL.sh
@@ -1,16 +1,49 @@
 #!/bin/bash
-cd ~
+
+#                         nmapSL
+#                    Created By Hi_skid
+#
+# This script will detect all open ports on the target and then
+# run service detection on those specific ports, cutting down on
+# The total time it takes to enumerate services on open ports.
+
+
+
+#Creating directory to save temporary files
+mkdir ~/nmapSL_RUN
+cd ~/nmapSL_RUN
+
+#Prompting user for input details regarding scan
+#Asking for hostname/IP, and saving results to the $hastname variable
 printf "\nThis script will scan the provided IP/Hostname over all possible ports and return the open ports, services running on those ports, and perform OS detection on the host.\n\nScan type is NMAP DEFAULT\n\nOpen ports will be collected first with no service scanning, then the open scans will be specifically scanned for services running over those ports\n\n\n"
 read -p "Enter Target IP/Hostname: " hostname
+
+#defining function that will later be used to concatinate all open ports discovered, and seperate them by a comma so they can be passed into the service discovery scan 
 function join_by { local IFS="$1"; shift; echo "$*"; }
+
+#Defining array and string variables for later use
 arr=()
 ports=""
-for i in $(nmap -p 1-65535 $hostname| grep open|grep -o '[0-9]\+')
+runpath="$(pwd)"
+
+#Initial port detection scan. This scan will find open ports, taking the $hostname variable collected from the user earlier to designate the target
+nmap -oN ports.txt --top-ports 1000 $hostname
+
+#ports.txt is searched for the ports, then the port numbers are pulled out and added 1 by 1 into the array for joining
+for i in $(cat ports.txt | grep open | awk '{print $1}'|grep -Eo '[0-9]{1,4}') 
 do
-	for t in $i
-       	do 
-		arr+=( "$t" )
-	done
+		arr+=( "$i" )
 done
+
+#Ports have been added to the array, so the temp file is no longer required
+rm ports.txt
+
+#Updating the user, then taking the items in the array (individual port numbers seperated by spaces), and joining them together, seperated by commas. This string is then assigned to the $ports variable for use in the service detection scan
+printf "\n\nInitial port detection complete, checking services on the detected open ports\n\n"
 ports=$(join_by , "${arr[@]}")
-sudo nmap -sV -O -p $ports $hostname| grep -w 'open\|OS\|report' >> nmap_results.txt
+echo $ports
+
+#Service detection scan, taking $hostname for the target, and $ports as the ports to scan.
+nmap -sV -p $ports $hostname| grep -w 'open\|OS\|report' >> nmap_results.txt
+printf "\n\nScan complete! Results have been saved in the nmap_results.txt file stored at "
+echo "${runpath}"
