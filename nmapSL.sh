dbaf55f9abe7856a6ecfe87fd8b72328f6780d94
benjazzy <benjazzy64@gmail.com>
2019-08-21 16:49:26 -0400

    Added basic help text

--- nmapSL.sh
+++ nmapSL.sh
@@ -7,6 +7,27 @@
 # run service detection on those specific ports, cutting down on
 # The total time it takes to enumerate services on open ports.
 
+#Setting variables
+
+#Help text
+usage="$(basename "$0") [-h] -- This script will detect all open ports on the target and then run service detection on those specific ports.
+
+where:
+-h  show this help text"
+
+while getopts ":h" opt; do
+	case ${opt} in
+		h )
+			echo "$usage"
+			exit 0
+			;;
+		\? )
+			echo "Invalid Option: -$OPTARG" 1>&2
+			exit 1
+			;;
+	esac
+done
+shift $((OPTIND -1))
 
 
 #Creating directory to save temporary files
@@ -27,12 +48,13 @@ ports=""
 runpath="$(pwd)"
 
 #Initial port detection scan. This scan will find open ports, taking the $hostname variable collected from the user earlier to designate the target
-nmap -oN ports.txt --top-ports 1000 $hostname
+nmap -Pn -oN ports.txt --top-ports 1000 $hostname
 
 #ports.txt is searched for the ports, then the port numbers are pulled out and added 1 by 1 into the array for joining
 for i in $(cat ports.txt | grep open | awk '{print $1}'|grep -Eo '[0-9]{1,4}') 
 do
-		arr+=( "$i" )
+	echo $i
+	arr+=( "$i" )
 done
 
 #Ports have been added to the array, so the temp file is no longer required
